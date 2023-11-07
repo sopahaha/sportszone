e4db6fdf3d5c92bb01813e1412732c1340c01e36
sopa <88498914+sopahaha@users.noreply.github.com>
2023-11-07 09:59:20 -0300

    Add files via upload

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -5,20 +5,22 @@ import { Dribbble } from "react-bootstrap-icons";
 
 function NavBarC() {
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "#b3b3b3" }}>
+    <Navbar
+      expand="lg"
+      style={{ backgroundColor: "#b3b3b3", flexDirection: "column" }}
+    >
+      <h1>SportsZone</h1>
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <Dribbble />
-          SportsZone
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <div className="justify-content-end">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/login">Entrar</Nav.Link>
             <Nav.Link href="/cadastro">Cadastrar Produto</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
