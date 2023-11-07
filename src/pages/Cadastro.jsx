732c98389719ff18f3922462ecda72278facf7cb
sopa <88498914+sopahaha@users.noreply.github.com>
2023-11-07 10:48:22 -0300

    Add files via upload

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,15 +1,21 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { Button } from "react-bootstrap";
-import "./ProductForm.css"; 
+import "./ProductForm.css"; // Importe seu arquivo de estilo CSS aqui
 
 const ProductForm = () => {
   const [productName, setProductName] = useState("");
   const [productDescription, setProductDescription] = useState("");
+  const [productBrand, setProductBrand] = useState("");
+  const [manufactureDate, setManufactureDate] = useState("");
+  const [stockQuantity, setStockQuantity] = useState("");
 
   const cadastrarProduto = (e) => {
     e.preventDefault();
     console.log("Nome do Produto: " + productName);
     console.log("Descrição do Produto: " + productDescription);
+    console.log("Marca do Produto: " + productBrand);
+    console.log("Data de Fabricação: " + manufactureDate);
+    console.log("Quantidade em Estoque: " + stockQuantity);
   };
 
   return (
@@ -33,6 +39,39 @@ const ProductForm = () => {
             value={productDescription}
             onChange={(e) => {
               setProductDescription(e.target.value);
+            }}
+            className="form-control"
+          />
+        </label>
+        <label>
+          <span>Marca do Produto</span>
+          <input
+            type="text"
+            value={productBrand}
+            onChange={(e) => {
+              setProductBrand(e.target.value);
+            }}
+            className="form-control"
+          />
+        </label>
+        <label>
+          <span>Data de Fabricação</span>
+          <input
+            type="date"
+            value={manufactureDate}
+            onChange={(e) => {
+              setManufactureDate(e.target.value);
+            }}
+            className="form-control"
+          />
+        </label>
+        <label>
+          <span>Quantidade em Estoque</span>
+          <input
+            type="number"
+            value={stockQuantity}
+            onChange={(e) => {
+              setStockQuantity(e.target.value);
             }}
             className="form-control"
           />
